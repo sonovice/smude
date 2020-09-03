147997b170035bbfe4fece192afe2ced548b0797
Simon Waloschek <simon.waloschek@posteo.de>
2020-09-03 11:46:16 +0200

    Removed dead code

--- smude/utils.py
+++ smude/utils.py
@@ -14,25 +14,6 @@ from scipy.misc import derivative
 from scipy.optimize import fsolve
 from typing_extensions import Protocol
 
-import progressbar
-
-
-class ProgressBar():
-    def __init__(self):
-        self.pbar = None
-
-    def __call__(self, block_num, block_size, total_size):
-        if not self.pbar:
-            self.pbar = progressbar.ProgressBar(maxval=total_size)
-            self.pbar.start()
-
-        downloaded = block_num * block_size
-        if downloaded < total_size:
-            self.pbar.update(downloaded)
-        else:
-            self.pbar.finish()
-
-
 
 class Derivable(Protocol):
     def __call__(self, val: float, derivative: Optional[int] = None) -> np.ndarray: ...
