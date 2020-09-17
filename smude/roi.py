601ec734842a2367c5c57aa13c1ce963a067fe7c
Simon Waloschek <simon.waloschek@posteo.de>
2020-09-17 15:01:38 +0200

    Reintroduced Felzenszwalb algo for segmentation

--- smude/roi.py
+++ smude/roi.py
@@ -5,7 +5,9 @@ from typing import Tuple
 import cv2
 import numpy as np
 from scipy import ndimage
+from skimage.exposure import equalize_adapthist
 from skimage.morphology import square
+from skimage.segmentation import felzenszwalb
 
 
 def get_border(image: np.ndarray) -> Tuple[int, int, int, int]:
@@ -75,11 +77,10 @@ def extract_roi_mask(image: np.ndarray, min_hull_ratio: float = 0.4) -> Tuple[np
     image_resized = cv2.resize(image, (size, size))
 
     image_gray = cv2.cvtColor(image_resized, cv2.COLOR_RGB2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=0.01, tileGridSize=(64, 64))
-    image_eq = clahe.apply(image_gray)
+    image_eq = equalize_adapthist(image_gray) * 255
     image_canny = auto_canny(image_eq.astype(np.uint8))
     image_canny = cv2.morphologyEx(image_canny, cv2.MORPH_DILATE, kernel=square(2))
-    _, image_segmented = cv2.connectedComponents(image_canny, connectivity=4)
+    image_segmented = felzenszwalb(image_canny, scale=1000, sigma=0.3, min_size=50)
 
     segment_sizes = np.bincount(image_segmented.flatten())
     segments = np.argsort(-segment_sizes)
@@ -94,6 +95,7 @@ def extract_roi_mask(image: np.ndarray, min_hull_ratio: float = 0.4) -> Tuple[np
         hull_opened = cv2.morphologyEx(hull.astype(np.uint8), cv2.MORPH_OPEN, kernel=square(20))
 
         # Take center blob
+        #blobs_segmented = measure.label(hull_opened)
         _, blobs_segmented = cv2.connectedComponents(hull_opened, connectivity=4)
         center_blob_label = blobs_segmented[size // 2, size // 2]
         hull = blobs_segmented == center_blob_label
@@ -109,6 +111,9 @@ def extract_roi_mask(image: np.ndarray, min_hull_ratio: float = 0.4) -> Tuple[np
 
     # Resize mask back to original image size
     mask_fullsize = cv2.resize(hull.astype(np.uint8), (height, width))
+
+    # Remove outer pixels so that dark residual pixels are removed
+    mask_fullsize = cv2.morphologyEx(mask_fullsize, cv2.MORPH_ERODE, kernel=square(25))
     mask_ratio = np.sum(hull) / (size**2)
 
     return mask_fullsize, mask_ratio
